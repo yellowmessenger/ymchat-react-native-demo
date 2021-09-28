7364104f7432ecbb95ce7f35d4d5f9c310052805
PurushYM <purushottam.yadav@yellowmessenger.com>
2021-09-28 13:39:10 +0530

    Updaed library, added sample code for unlink device token

--- App.js
+++ App.js
@@ -7,21 +7,37 @@
  */
 
 import React from 'react';
-import {Button, View, StyleSheet} from 'react-native';
-import {YMChat} from 'ymchat-react-native';
+import { Button, View, StyleSheet } from 'react-native';
+import { YMChat } from 'ymchat-react-native';
+
+let deviceToken = "deviceToken";
+let apiKey = "apiKey";
+let botId = "x1608615889375";
 
 const App: () => Node = () => {
   const onPress = () => {
-    YMChat.setBotId('x1568547207370'); // Default Bot Id
+    YMChat.setBotId(botId); // Default Bot Id
     YMChat.setEnableHistory(true);
     // ... OTHER Configurations. (refer: https://github.com/yellowmessenger/ymchat-react-native)
     YMChat.startChatbot();
   };
 
+  const logout = () => {
+    YMChat.unlinkDeviceToken(botId, apiKey, deviceToken, (unLinkDeviceTokenResult) => {
+      if (unLinkDeviceTokenResult == true) {
+        console.log("Device token unlinked");
+      }
+      else {
+        console.log(unLinkDeviceTokenResult);
+      }
+    });
+  }
+
   return (
     <>
       <View style={styles.fixToText}>
         <Button title="Start Chatbot" onPress={onPress} />
+        <Button title="Unlink device token" onPress={logout} />
       </View>
     </>
   );
