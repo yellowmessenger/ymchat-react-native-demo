a40455a40626e6bf90c454dd4dc693c9d0512dc3
Sankalp Gupta <sankalpgupta021193@gmail.com>
2021-11-18 11:27:54 +0530

    Requesting FCM token after APN Token is generated

--- ios/react_native_demo_app/AppDelegate.m
+++ ios/react_native_demo_app/AppDelegate.m
@@ -52,7 +52,31 @@ static void InitializeFlipper(UIApplication *application) {
   rootViewController.view = rootView;
   self.window.rootViewController = rootViewController;
   [self.window makeKeyAndVisible];
+  
+  [UNUserNotificationCenter currentNotificationCenter].delegate = self;
+  UNAuthorizationOptions authOptions = UNAuthorizationOptionAlert |
+  UNAuthorizationOptionSound | UNAuthorizationOptionBadge;
+  [[UNUserNotificationCenter currentNotificationCenter]
+   requestAuthorizationWithOptions:authOptions
+   completionHandler:^(BOOL granted, NSError * _Nullable error) {
+    // ...
+  }];
+  
+  [application registerForRemoteNotifications];
   return YES;
+}
+
+- (void)application:(UIApplication *)application
+didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken {
+  [FIRMessaging messaging].APNSToken = deviceToken;
+  
+  [[FIRMessaging messaging] tokenWithCompletion:^(NSString *token, NSError *error) {
+    if (error != nil) {
+      NSLog(@"Error getting FCM registration token: %@", error);
+    } else {
+      NSLog(@"FCM registration token: %@", token);
+    }
+  }];
 }
 
 - (NSURL *)sourceURLForBridge:(RCTBridge *)bridge
