50faa8a1911844bd05b8982fac5937f122630e49
Rohit Kumar <123462379+rohit-131@users.noreply.github.com>
2024-02-26 13:24:13 +0530

    AppDelegate.mm updated

--- ios/ymchat_react_native_demo/AppDelegate.mm
+++ ios/ymchat_react_native_demo/AppDelegate.mm
@@ -1,4 +1,5 @@
 #import "AppDelegate.h"
+#import "Firebase.h"
 
 #import <React/RCTBundleURLProvider.h>
 
@@ -6,12 +7,39 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+  [FIRApp configure];
+  [UNUserNotificationCenter currentNotificationCenter].delegate = self;
+      UNAuthorizationOptions authOptions = UNAuthorizationOptionAlert |
+      UNAuthorizationOptionSound | UNAuthorizationOptionBadge;
+      [[UNUserNotificationCenter currentNotificationCenter]
+       requestAuthorizationWithOptions:authOptions
+       completionHandler:^(BOOL granted, NSError * _Nullable error) {
+           if (!error) {
+               NSLog(@"User Notification permission granted: %d", granted);
+           }
+       }];
+      
+      [application registerForRemoteNotifications];
+  
   self.moduleName = @"ymchat_react_native_demo";
   // You can add your custom initial props in the dictionary below.
   // They will be passed down to the ViewController used by React Native.
   self.initialProps = @{};
 
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
+}
+
+- (void)application:(UIApplication *)application
+didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken {
+  [FIRMessaging messaging].APNSToken = deviceToken;
+  
+  [[FIRMessaging messaging] tokenWithCompletion:^(NSString *token, NSError *error) {
+    if (error != nil) {
+      NSLog(@"Error getting FCM registration token: %@", error);
+    } else {
+      NSLog(@"FCM registration token: %@", token);
+    }
+  }];
 }
 
 - (NSURL *)sourceURLForBridge:(RCTBridge *)bridge
